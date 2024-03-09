78da77e100331a7d9acfa7920683d2fdbc70ade7
luttje <2738114+luttje@users.noreply.github.com>
2024-03-09 07:51:45 +0100

    copy ispconfig version too

--- vite.config.js
+++ vite.config.js
@@ -10,57 +10,63 @@ const srcTemplatesPath = path('src/templates');
 const liquidish = new LiquidishTransformer({});
 
 const staticAssetsDirectories = [
-  'favicon',
-  'fonts',
-  'images',
-  'javascripts',
-  'stylesheets',
+    'favicon',
+    'fonts',
+    'images',
+    'javascripts',
+    'stylesheets',
 ];
 
 export default defineConfig({
-  build: {
-    outDir: path('dist'),
+    build: {
+        outDir: path('dist'),
 
-    // Build as lib, so we can include manually
-    lib: {
-      entry: path('src/assets/app.js'),
-      name: 'main',
-      fileName: (format) => `tailwindone.${format}.js`,
-    },
-
-    css: {
-      postcss: {
-        plugins: [tailwindcss],
-      },
-    },
-
-    // Output JS and CSS to the assets directory
-    rollupOptions: {
-      output: {
-        dir: path('dist/assets'),
-      },
-    },
-  },
-
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/templates/**/*.liquid',
-          dest: 'templates',
-
-          transform: (contents, path) => liquidish.transform(contents, path),
-
-          rename: function (name, ext, fullPath) {
-            const path = fullPath.replace(srcTemplatesPath, '');
-            return path.replace(/\.liquid$/, '.htm');
-          },
+        // Build as lib, so we can include manually
+        lib: {
+            entry: path('src/assets/app.js'),
+            name: 'main',
+            fileName: (format) => `tailwindone.${format}.js`,
         },
-        ...staticAssetsDirectories.map((dir) => ({
-          src: `src/assets/${dir}`,
-          dest: `assets`,
-        })),
-      ],
-    })
-  ],
+
+        css: {
+            postcss: {
+                plugins: [tailwindcss],
+            },
+        },
+
+        // Output JS and CSS to the assets directory
+        rollupOptions: {
+            output: {
+                dir: path('dist/assets'),
+            },
+        },
+    },
+
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/templates/**/*.liquid',
+                    dest: 'templates',
+
+                    transform: (contents, path) => liquidish.transform(contents, path),
+
+                    rename: function (name, ext, fullPath) {
+                        const path = fullPath.replace(srcTemplatesPath, '');
+                        return path.replace(/\.liquid$/, '.htm');
+                    },
+                },
+
+                ...staticAssetsDirectories.map((dir) => ({
+                    src: `src/assets/${dir}`,
+                    dest: `assets`,
+                })),
+
+                {
+                    src: 'src/ispconfig_version',
+                    dest: '.',
+                },
+            ],
+        })
+    ],
 })
